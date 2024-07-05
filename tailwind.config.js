79f9ad843b6c88c224922b1c3c8e3cd26022178b
tareqhasan382 <tareqhasan382@gmail.com>
2024-07-06 02:42:54 +0600

    done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        boska: ["BOSKA", "sans-serif"],
+        boskaItalic: ["BOSKA-ITALIC", "sans-serif"],
+        switzer: ["SWITZER", "sans-serif"],
+        poppins: ["POPPINS", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
